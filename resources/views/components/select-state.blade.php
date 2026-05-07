b5700ad72a106f83a68fe490b9afaf4ce6984b95
Edgardo Ramirez <edgardo.ramirez@nyu.edu>
2026-05-07 12:36:29 -0400

    Updating views

--- resources/views/components/select-state.blade.php
+++ resources/views/components/select-state.blade.php
@@ -1,5 +1,5 @@
 <select id="stateSelect" name="state_id">
-    <option value="">State/Region</option>
+    <option value="">Departamento</option>
     @foreach ($states as $state )
         <option value="{{ $state->id }}"
             @selected($attributes->get('value') == $state->id)>{{ $state->name }}</option>
